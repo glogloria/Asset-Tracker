ab364b40b2c4ff91abee906100dea90933f22662
glogloria <140434371+glogloria@users.noreply.github.com>
2023-08-12 17:11:03 -0400

    Update Open.py

--- Open.py
+++ Open.py
@@ -1,12 +1,4 @@
-Windows PowerShell
-Copyright (C) Microsoft Corporation. All rights reserved.
+pip install openpyxl
+#Dw
 
-Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
-
-PS C:\Users\thegl> pip install openpyxl
-Requirement already satisfied: openpyxl in c:\users\thegl\appdata\local\packages\pythonsoftwarefoundation.python.3.11_qbz5n2kfra8p0\localcache\local-packages\python311\site-packages (3.1.2)
-Requirement already satisfied: et-xmlfile in c:\users\thegl\appdata\local\packages\pythonsoftwarefoundation.python.3.11_qbz5n2kfra8p0\localcache\local-packages\python311\site-packages (from openpyxl) (1.1.0)
-
-[notice] A new release of pip is available: 23.1.2 -> 23.2.1
-[notice] To update, run: C:\Users\thegl\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\python.exe -m pip install --upgrade pip
-PS C:\Users\thegl>
+Import open
