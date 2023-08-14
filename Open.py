fd3534f02198b00136dc7df903e783bb77fd195f
glogloria <140434371+glogloria@users.noreply.github.com>
2023-08-14 14:43:15 -0400

    Working.py

--- Open.py
+++ Open.py
@@ -1,4 +1,10 @@
-pip install openpyxl
-#Dw
+#Greeting
+print("Hello! I am ESG Asset Bot. I can provide assets from one of four Dell signature programs (Girls Who Game, ProBono, Student TechCrew, Solar Community Hubs")
 
-Import open
+#Assets
+signProgram = input("Which signature program are you interested in?\n")
+print("Great! What kind of " + signProgram + " assets are you looking for?")
+giveInfo = input("Enter the letter for the following options: \n(a) Customer impact stories, (b) Images, (c) Testimonials" \n)
+
+print("Enter the letter for the following options: \n(a) Customer impact stories, (b) Images, (c) Testimonials")
+  topic = input()
